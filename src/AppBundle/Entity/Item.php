506ad3e259dd45a638d28575d3f5cd5548edb623
Никулин Андрей <turkishjoe283@gmail.com>
2017-08-27 01:11:23 +0300

    master - add item entity

---
+++ src/AppBundle/Entity/Item.php
@@ -0,0 +1,137 @@
+<?php
+
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="itemss")
+ */
+class Item
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=127)
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="text")
+     *
+     * @var string
+     */
+    protected $description;
+
+
+    /**
+     * @ORM\Column(type="string", length=127)
+     *
+     * @var string
+     */
+    protected $period;
+
+
+    /**
+     * TODO:Add your description
+     * @ORM\Column(type="datetime")
+     *
+     */
+    protected $createTime;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPeriod()
+    {
+        return $this->period;
+    }
+
+    /**
+     * @param string $period
+     */
+    public function setPeriod($period)
+    {
+        $this->period = $period;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreateTime()
+    {
+        return $this->createTime;
+    }
+
+    /**
+     * @param mixed $createTime
+     */
+    public function setCreateTime($createTime)
+    {
+        $this->createTime = $createTime;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+
+}
